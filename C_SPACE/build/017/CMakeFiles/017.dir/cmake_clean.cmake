54289039c7b6a8ed65431e5c95c3336c20e9537b
lixiaosong <1531503999@qq.com>
2022-11-19 18:41:07 +0800

    build project make success.

---
+++ C_SPACE/build/017/CMakeFiles/017.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/017.dir/letterCombinations.c.obj"
+  "lib017.a"
+  "lib017.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/017.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_SPACE/build/017/CMakeFiles/017.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "lib017.a"
+)
